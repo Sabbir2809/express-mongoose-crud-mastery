97bf447455cacd27079f1ff4c0e5dc6f79eaa3a6
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2023-11-23 23:44:19 +0600

    [interface]: create IUser Interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,30 @@
+type FullName = {
+  firstName: string;
+  lastName: string;
+};
+
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+type Order = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+interface IUser {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: FullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: Address;
+  orders: Order[];
+}
+export default IUser;
